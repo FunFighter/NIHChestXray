cda055d58867dc4c12c5c2a57be241564984770a
FunFighter <42244855+FunFighter@users.noreply.github.com>
2019-08-10 12:52:15 -0700

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -2,43 +2,69 @@ from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 
+import numpy as np
+
 import keras
 from keras.preprocessing import image
 from keras import backend as K
 
+import pickle
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 model = None
 graph = None
+img = None
 
+all_labels = ['Atelectasis',
+                'Cardiomegaly',
+                'Consolidation',
+                'Edema',
+                'Effusion',
+                'Emphysema',
+                'Fibrosis',
+                'Hernia', 
+                'Infiltration',
+                'Mass',
+                'Nodule',
+                'Pleural_Thickening',
+                'Pneumonia',
+                'Pneumothorax']
 
 # Loading a keras model with flask
-# https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
+# https://towardsdatascience.com/deploying-keras-deep-learning-models-with-flask-5da4181436a2
 def load_model(ModelName):
     global model
     global graph
-    model = keras.models.load_model(f'{ModelName}.h5')
-    graph = K.get_session().graph
+    model = keras.models.load_model(f'./Models/{ModelName}.h5')
 
 
-# load_model()
 
-def prepare_image(img):
-    # Convert the image to a numpy array
-    img = image.img_to_array(img)
-    # Scale from 0 to 255
-    img /= 255
-    # Flatten the image to an array of pixels
-    image_array = img.flatten().reshape(-1, 230 * 230)
-    # Return the processed feature array
-    return image_array
+
+# for label in all_labels:
+#     load_model(label)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {"success": False}
+    # get the request parameters
+    params = request.json
+    if (params == None):
+        params = request.args
+    # if parameters are found, echo the msg parameter 
+    if (params != None):
+        data["response"] = params.get("msg")
+        data["success"] = True
+    # return a response in json format 
+    return jsonify(data)
+
 
 @app.route('/submit', methods = ['GET', 'POST'])
 def submit():
@@ -61,27 +87,30 @@ def submit():
 
             # Load the saved image using Keras and resize it to the mnist
             # format of 230x230 pixels
-            image_size = (230, 230)
-            im = image.load_img(filepath, target_size=image_size,
-                                grayscale=True)
-
-            # Convert the 2D image to an array of pixel values
-            image_array = prepare_image(im)
-            print(image_array)
+            img = image.load_img(filepath, target_size=(230, 230))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            images = np.vstack([x])
+            predicted_digit = model.predict(images, batch_size=10)
+     
+            print(predicted_digit)
 
             # Get the tensorflow default graph and use it to make predictions
-            # global graph
-            # with graph.as_default():
+            
+            
 
-            #     # Use the model to make a prediction
-            #     predicted_digit = model.predict_classes(image_array)[0]
-            #     data["prediction"] = str(predicted_digit)
+            # Use the model to make a prediction
+            
+            data["prediction"] = predicted_digit
 
-            #     # indicate that the request was a success
-            #     data["success"] = True
+            # indicate that the request was a success
+            data["success"] = True
 
-            return '''<div>test  testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</div> '''
+            return render_template('submit.html',dataStuff = data["prediction"])
+
     return render_template('submit.html')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   load_model("Cardiomegaly")
+   model._make_predict_function()
+   app.run(debug = True ,port=5555)
